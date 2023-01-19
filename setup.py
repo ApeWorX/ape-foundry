5cc34dc35594daea30fb29e38f43e67b5c7c4e76
unparalleled-js <yingthi@live.com>
2023-01-19 10:15:37 -0600

    chore: updates from template

--- setup.py
+++ setup.py
@@ -20,6 +20,9 @@ extras_require = {
         "types-PyYAML",  # Needed due to mypy typeshed
         "flake8>=5.0.4",  # Style linter
         "isort>=5.10.1",  # Import sorting linter
+        "mdformat>=0.7.16",  # Auto-formatter for markdown
+        "mdformat-gfm>=0.3.5",  # Needed for formatting GitHub-flavored markdown
+        "mdformat-frontmatter>=0.4.1",  # Needed for frontmatters-style headers in issue templates
     ],
     "doc": [
         "Sphinx>=3.4.3,<4",  # Documentation generator

a462bc6cc153899c069192ac6e8b72f4a712ca9c
unparalleled-js <yingthi@live.com>
2023-01-23 14:52:28 -0600

    fix: fix pytester tests

--- tests/test_gas_report.py
+++ tests/test_gas_report.py
@@ -48,6 +48,7 @@ def filter_expected_methods(*methods_to_remove: str) -> str:
 def ape_pytester(project, pytester):
     pytester.makeconftest(CONFTEST)
     pytester.makepyfile(TEST_FILE)
+    project._cached_projects = {}
     return pytester
 
 

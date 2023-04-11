f8595817f9c540837a2b7d66732c1ba62b5a0ba8
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2023-04-10 21:37:34 -0400

    docs: update

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os.path
 
 from setuptools import find_packages, setup
 
@@ -53,11 +52,8 @@ extras_require["dev"] = (
     + extras_require["dev"]
 )
 
-readme_path, readme_content_type = "./README.md", "text/x-rst"
-if os.path.exists("./README.md"):
-    readme_path, readme_content_type = "./README.md", "text/markdown"
 
-with open(readme_path) as readme:
+with open("./README.md") as readme:
     long_description = readme.read()
 
 
@@ -67,7 +63,7 @@ setup(
     setup_requires=["setuptools_scm"],
     description="""ape-foundry: Ape network provider for Foundry""",
     long_description=long_description,
-    long_description_content_type=readme_content_type,
+    long_description_content_type="text/markdown",
     author="ApeWorX Ltd.",
     author_email="admin@apeworx.io",
     url="https://github.com/ApeWorX/ape-foundry",
